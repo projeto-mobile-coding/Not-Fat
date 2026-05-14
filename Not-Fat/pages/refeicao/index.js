6b3f93d66a0cff4131d84fa122d02c2485786fce
Everton Matias <everton7matias@gmail.com>
2026-05-13 23:03:11 -0300

    feat: Início do desenvolvimento da tab bar e da tela de refeições.
    Foi instalado a biblioteca react-navigation e a partição bottom-tabs da biblioteca mencionada.
    Foi iniciado a montagem com o esqueleto da página de refeições.

--- Not-Fat/pages/refeicao/index.js
+++ Not-Fat/pages/refeicao/index.js
@@ -1,1 +1,40 @@
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  Platform,
+} from "react-native";
+import { refeicaoNome } from "../../components/constantes/refeicao-nome";
+import { styles } from "./style";
+
+export default function Refeicao() {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <StatusBar style="light" />
+          <View>
+            <Text> Área inicial</Text>
+            <Text>Local das refeições</Text>
+            <View>
+              <Text>Início</Text>
+            </View>
+            <View>
+              <Text>Refeições</Text>
+              <View /* Ficará o espaço para as refeições */>
+                {/* {refeicaoNome.map(refeicao) => ()} */}
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
